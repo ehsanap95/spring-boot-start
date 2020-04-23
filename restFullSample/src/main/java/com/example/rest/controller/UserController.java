2ab1256797c0209d0bdb964bad670d5bf23a395d
ehsanap95 <ehsanap95@ubuntu>
2020-04-24 00:08:25 +0430

    Start Spring Boot

---
+++ restFullSample/src/main/java/com/example/rest/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.rest.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rest.object.Person;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("/getUserDetails")
+	public String getUserDetail(@RequestHeader String name) {
+		return "Get All Users" + name;
+	}
+	
+	@PostMapping("/createUser")
+	public Person createUser(@RequestHeader String name) {
+		return new Person("1",name,"12");
+	}
+	
+	@PutMapping("/updateUser")
+	public Person updateUser(@RequestBody Person person) {
+		return person;
+	}
+	
+	@DeleteMapping("/deleteUser")
+	public String deleteUser() {
+		return "Delete User";
+	}
+	
+}
