3bbd592421db9dc09cc68db59bb4100429368e1e
ehsanap95 <ehsanap95@ubuntu>
2020-06-04 18:08:22 +0430

    List For Rest

--- melody1music/src/main/java/com/melody1music/project/music/controller/ArtistController.java
+++ melody1music/src/main/java/com/melody1music/project/music/controller/ArtistController.java
@@ -1,9 +1,12 @@
 package com.melody1music.project.music.controller;
 
 import com.melody1music.project.music.dto.ArtistDTO;
+import com.melody1music.project.music.dto.PersonDTO;
 import com.melody1music.project.music.service.inter.IArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +23,13 @@ public class ArtistController {
     public List<ArtistDTO> getAllArtists() {
         return iArtistService.findAll();
     }
+    
+	@PutMapping("/groupArtist")
+	public String updatePerson(@RequestBody List<ArtistDTO> artistDTOs) {
+		for (ArtistDTO artistDTO : artistDTOs) {
+			System.out.println(artistDTO.getFullName());
+		}
+		return "Get It";
+	}
+    
 }
